69e8afc3d4e32fb9e308ee62fb88eff0422544a3
= <=>
2026-09-21 14:00:19 +0200

    broken test (ch 2,2)

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+// GetAPIKey -
+func TestGETAPIKey(t *testing.T) {
+	headers := http.Header{}
+
+	headers.Set("Authorization", "ApiKey abc123")
+	got, err := GetAPIKey(headers)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	want := "abc123!"
+	if got != want {
+		t.Fatalf("got: %q, want %q", got, want)
+	}
+}
